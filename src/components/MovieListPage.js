2c7daea3105c2848a7c497bbfeacf7997a791854
Rishabh Raj <rajrishabh225@gmail.com>
2024-05-27 20:32:23 +0530

    adding video link of google meet

--- src/components/MovieListPage.js
+++ src/components/MovieListPage.js
@@ -31,12 +31,14 @@ const MovieListPage = () => {
         const isFavorite = favorites.includes(movieId);
         if (isFavorite) {
             dispatch(removeFromFavorites(movieId));
+            const updatedFavorites = [...favorites, movieId];
+            localStorage.removeItem('favorites', JSON.stringify(updatedFavorites));
         } else {
             dispatch(addToFavorites(movieId));
+            const updatedFavorites = [...favorites, movieId];
+            localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
         }
 
-        const updatedFavorites = [...favorites, movieId];
-        localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
     };
 
     useEffect(() => {
@@ -53,22 +55,22 @@ const MovieListPage = () => {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {movies.map((movie) => (
                     <div key={movie.id} className='relative'>
-                    <a href={movie.imdb_url} target="_blank" rel="noopener noreferrer" className="border z-0 bg-gradient-to-br from-cyan-100 to via-red-100 bg-yellow-300 rounded-lg shadow-lg p-4 block hover:scale-105 duration-300 hover:via-cyan-300 h-40">
-                        <div>
-                            <img src={movie.image} alt={movie.movie} className="w-full h-auto mb-2 rounded-lg" />
-                        </div>
-                        <div>
-                            <h2 className="text-lg font-bold">{movie.movie}</h2>
-                            <p className="text-sm">Rating: {movie.rating}</p>
-                        </div>
-                    </a>
-                    <button
-                    onClick={() => handleToggleFavorite(movie.id)}
-                    className="absolute bottom-0 right-0 z-20 text-red-800 text-xl px-4 py-2 rounded mt-4"
-                >
-                    {favorites.includes(movie.id) ? <FaHeart /> : <FaRegHeart />}
-                </button>
-                </div>
+                        <a href={movie.imdb_url} target="_blank" rel="noopener noreferrer" className="border z-0 bg-gradient-to-br from-cyan-100 to via-red-100 bg-yellow-300 rounded-lg shadow-lg p-4 block hover:scale-105 duration-300 hover:via-cyan-300 h-40">
+                            <div>
+                                <img src={movie.image} alt={movie.movie} className="w-full h-auto mb-2 rounded-lg" />
+                            </div>
+                            <div>
+                                <h2 className="text-lg font-bold">{movie.movie}</h2>
+                                <p className="text-sm">Rating: {movie.rating}</p>
+                            </div>
+                        </a>
+                        <button
+                            onClick={() => handleToggleFavorite(movie.id)}
+                            className="absolute bottom-0 right-0 z-20 text-red-800 text-xl px-4 py-2 rounded mt-4"
+                        >
+                            {favorites.includes(movie.id) ? <FaHeart /> : <FaRegHeart />}
+                        </button>
+                    </div>
                 ))}
             </div>
         </div>
